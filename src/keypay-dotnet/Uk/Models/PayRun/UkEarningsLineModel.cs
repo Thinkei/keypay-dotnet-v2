fb8c433f2400e4bc96b6917f97880222704c49a5
ci <ci@webscale.com.au>
2026-09-07 23:46:45 +0000

    Build 3.0.0.977 updates

--- src/keypay-dotnet/Uk/Models/PayRun/UkEarningsLineModel.cs
+++ src/keypay-dotnet/Uk/Models/PayRun/UkEarningsLineModel.cs
@@ -18,6 +18,7 @@ namespace KeyPayV2.Uk.Models.PayRun
         public decimal EmployeePensionContribution { get; set; }
         public decimal EmployerPensionContribution { get; set; }
         public bool PayRateNetPaymentEarningsLineDeleted { get; set; }
+        public decimal? NetToGrossDesiredNetPaymentAmount { get; set; }
         public string PayCategoryId { get; set; }
         public string PayCategoryName { get; set; }
         public decimal Units { get; set; }
